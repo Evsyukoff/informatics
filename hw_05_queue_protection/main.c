5d605986a6cf1dd61116c7add7daa966cc89ac67
evsyukoff <evsyukoffdanila25@gmail.com>
2020-06-25 18:16:56 +0300

    Create main.c
    summary

---
+++ hw_05_queue_protection/main.c
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+int main(void) {
+    queue_t queue; 
+    queue.head = NULL; 
+    queue.tail = NULL;
+
+    int value;
+    scanf("%d", &value); 
+    int last_value = -1;
+
+    while (value != -1) {
+        if (value != 0) {
+            if (last_value == 0) {
+                printf("%d ", value);
+            }
+        }
+        else if (last_value != 0) {
+            enqueue(&queue, last_value);
+        }
+
+        last_value = value; 
+        scanf("%d", &value); 
+    }
+
+    printf("\n");
+
+    while (!is_empty(queue))
+        printf("%d ", dequeue(&queue));
+
+    printf("\n");
+
+    return 0;
+}
